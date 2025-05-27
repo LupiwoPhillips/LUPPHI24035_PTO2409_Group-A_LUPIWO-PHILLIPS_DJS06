de003fca5fcc268550754d2b71b769ae52580df6
Lupiwo Phillips <lupiwonp@gmail.com>
2025-05-27 22:35:32 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,3 +15,11 @@ const products = [
 ]
 
 // 1. **ForEach Basics** 
+
+// log each name and each province to the console
+
+provinces.forEach(provinces => console.log(provinces));
+names.forEach(names => console.log(names));
+
+// log each name with a matching province in the format "Name (Province)"
+
